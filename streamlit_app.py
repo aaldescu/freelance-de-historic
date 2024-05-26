6ecf66646d2b5fe308e2ce7f55cef5c26a786e86
Andrei Aldescu <andrei.aldescu@yahoo.com>
2024-05-27 00:17:44 +0200

    use altair to better display the line charts

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,14 +3,14 @@ import os
 import streamlit as st
 from datetime import timedelta
 import math
+import altair as alt
 
 def safe_divide_and_ceil(numerator, denominator):
     if denominator == 0:  # Check for division by zero
         return 0  # Or None, or any other value you deem appropriate
     else:
         result = numerator / denominator
-        #return math.ceil(result)
-        return round(result,2)
+        return round(result, 2)
     
 project_path = os.path.dirname(os.path.realpath(__file__))
 
@@ -32,7 +32,7 @@ data_frames = [pd.read_csv(file, sep=';').assign(source=os.path.basename(file)) 
 # Concatenate all DataFrames in the list into a single DataFrame
 df = pd.concat(data_frames, ignore_index=True)
 
-#Streamlit Dashboard
+# Streamlit Dashboard
 
 # Convert date column to datetime
 df['date'] = pd.to_datetime(df['date']).dt.floor('D')
@@ -62,9 +62,9 @@ start_date, end_date = pd.to_datetime(selected_date_range[0]), pd.to_datetime(se
 filtered_df = df[(df['date'] >= start_date) & (df['date'] <= end_date)]
 filtered_df = filtered_df[filtered_df['job_group'].isin(selected_job_groups)]
 
-#Split data soruce into job and expert
-job_filtered_df = filtered_df[ filtered_df['source'].isin(['freelancermap_project_data.csv', 'project_data.csv'])]
-expert_filtered_df = filtered_df[ filtered_df['source'].isin(['freelance_data.csv'])]
+# Split data source into job and expert
+job_filtered_df = filtered_df[filtered_df['source'].isin(['freelancermap_project_data.csv', 'project_data.csv'])]
+expert_filtered_df = filtered_df[filtered_df['source'].isin(['freelance_data.csv'])]
 
 # Preparing data for the line chart
 job_pivot_df = job_filtered_df.pivot_table(index='date', columns='job_group', values='num_jobs', aggfunc='sum').fillna(0)
@@ -72,12 +72,11 @@ expert_pivot_df = expert_filtered_df.pivot_table(index='date', columns='job_grou
 
 # Data for Experts / Jobs Ratio
 df_pivot = filtered_df.pivot_table(index=["job_group", "date"], columns="source", values="num_jobs").fillna(0).reset_index()
-df_pivot["experts"] = df_pivot["freelance_data.csv"]+df_pivot["freelancermap_project_data.csv"]
-df_pivot = df_pivot.drop(columns=["freelance_data.csv","freelancermap_project_data.csv"])
+df_pivot["experts"] = df_pivot["freelance_data.csv"] + df_pivot["freelancermap_project_data.csv"]
+df_pivot = df_pivot.drop(columns=["freelance_data.csv", "freelancermap_project_data.csv"])
 # Apply the function for safe division and rounding up
 df_pivot["ratio"] = df_pivot.apply(lambda row: safe_divide_and_ceil(row["experts"], row["project_data.csv"]), axis=1)
 expert_ratio_df = df_pivot.pivot_table(index='date', columns='job_group', values='ratio', aggfunc='sum').fillna(0)
-
 
 # Calculate the daily differences for jobs
 job_daily_diff = job_pivot_df.diff().fillna(0)  # Calculate day-to-day difference and fill NaN with 0
@@ -85,33 +84,66 @@ job_daily_diff = job_pivot_df.diff().fillna(0)  # Calculate day-to-day differenc
 # Calculate the daily differences for experts
 expert_daily_diff = expert_pivot_df.diff().fillna(0)  # Similar calculation for experts
 
-
 st.title('Germany\'s freelancer market in daily numbers')
 st.markdown(buy_me_a_coffee_html, unsafe_allow_html=True)
 
 st.header('Jobs :money_with_wings:')
-# Plotting with st.line_chart
-st.line_chart(job_pivot_df)
+
+# Plotting with st.altair_chart to rotate labels
+job_chart = alt.Chart(job_pivot_df.reset_index().melt('date')).mark_line().encode(
+    x=alt.X('date:T', axis=alt.Axis(labelAngle=45)), 
+    y='value:Q', 
+    color='job_group:N'
+).properties(
+    width=800,
+    height=400
+)
+
+st.altair_chart(job_chart)
 
 st.header('Experts per Job :sunglasses:')
-st.line_chart(expert_ratio_df)
+expert_ratio_chart = alt.Chart(expert_ratio_df.reset_index().melt('date')).mark_line().encode(
+    x=alt.X('date:T', axis=alt.Axis(labelAngle=45)), 
+    y='value:Q', 
+    color='job_group:N'
+).properties(
+    width=800,
+    height=400
+)
+
+st.altair_chart(expert_ratio_chart)
 
 # Visualize the daily differences
 st.header('Daily Difference in Jobs :chart_with_upwards_trend:')
-st.bar_chart(job_daily_diff)
+daily_diff_chart_jobs = alt.Chart(job_daily_diff.reset_index().melt('date')).mark_bar().encode(
+    x=alt.X('date:T', axis=alt.Axis(labelAngle=45)), 
+    y='value:Q', 
+    color='job_group:N'
+).properties(
+    width=800,
+    height=400
+)
+
+st.altair_chart(daily_diff_chart_jobs)
 
 st.header('Daily Difference in Experts :chart_with_upwards_trend:')
-st.bar_chart(expert_daily_diff)
+daily_diff_chart_experts = alt.Chart(expert_daily_diff.reset_index().melt('date')).mark_bar().encode(
+    x=alt.X('date:T', axis=alt.Axis(labelAngle=45)), 
+    y='value:Q', 
+    color='job_group:N'
+).properties(
+    width=800,
+    height=400
+)
 
+st.altair_chart(daily_diff_chart_experts)
 
-#
 df_topcomp = df.pivot_table(index=["job_group", "date"], columns="source", values="num_jobs").fillna(0).reset_index()
-df_topcomp["experts"] = df_topcomp["freelance_data.csv"]+df_topcomp["freelancermap_project_data.csv"]
-df_topcomp = df_topcomp.drop(columns=["freelance_data.csv","freelancermap_project_data.csv"])
+df_topcomp["experts"] = df_topcomp["freelance_data.csv"] + df_topcomp["freelancermap_project_data.csv"]
+df_topcomp = df_topcomp.drop(columns=["freelance_data.csv", "freelancermap_project_data.csv"])
 # Apply the function for safe division and rounding up
 df_topcomp["ratio"] = df_topcomp.apply(lambda row: safe_divide_and_ceil(row["experts"], row["project_data.csv"]), axis=1)
 
-#
 abc = df_topcomp.groupby('job_group')['ratio'].mean().round().reset_index()
 abc = abc.sort_values(by="ratio", ascending=False)
 st.write(abc)
